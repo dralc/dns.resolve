3003d9f58799decfb49847e00b56d983b12815f5
Aloysius <dr.aloysius@gmail.com>
2021-07-18 08:23:40 +0000

    add user msg

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
-const express = require('express');
-const dns = require('dns');
+const express = require('express')
+const dns = require('dns')
 
-const app = express();
+const app = express()
 
 app.get('/', (req, res) => {
   const {
     query: { domain = '' }
-  } = req;
+  } = req
 
-  dns.resolve(domain, 'A', (er, records) => {
-    if (er) {
-      console.error(er);
-    }
-    res.json(records);
-  });
+  if (domain) {
+    dns.resolve(domain, 'A', (er, records) => {
+      if (er) console.error(er)
+      res.json(records)
+    });
+  } else {
+    res.status(200).send('Add a url param "domain"')
+  }
 });
 
 app.listen(8080, () => {
-  console.log(`* listening`);
+  console.log(`* listening`)
 });
